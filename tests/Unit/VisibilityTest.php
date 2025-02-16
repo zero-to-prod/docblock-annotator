aa82afddc739060291609a77014f377723cfe191
david_smith <david_smith@sweetwater.com>
2025-02-16 09:40:05 -0500

    feat/annotator Created `tests/Unit/VisibilityTest.php`.

---
+++ tests/Unit/VisibilityTest.php
@@ -0,0 +1,144 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Zerotoprod\DocblockAnnotator\Annotator;
+
+class VisibilityTest extends TestCase
+{
+
+    /** @test */
+    public function adds_a_comment_public_member(): void
+    {
+        $file = <<<PHP
+        <?php
+        class User
+        {
+            public function method(): string
+            {
+                return '';
+            }
+        }
+        PHP;
+
+        $code = (new Annotator(['comment'], ['public']))->process($file);
+
+        self::assertEquals(
+            <<<PHP
+            <?php
+            class User
+            {
+                /**
+                 * comment
+                 */
+                public function method(): string
+                {
+                    return '';
+                }
+            }
+            PHP,
+            $code
+        );
+    }
+
+
+    /** @test */
+    public function does_not_add_a_comment_public_member(): void
+    {
+        $file = <<<PHP
+        <?php
+        class User
+        {
+            public function method(): string
+            {
+                return '';
+            }
+        }
+        PHP;
+
+        $code = (new Annotator(['comment'], ['private']))->process($file);
+
+        self::assertEquals(
+            <<<PHP
+            <?php
+            class User
+            {
+                public function method(): string
+                {
+                    return '';
+                }
+            }
+            PHP,
+            $code
+        );
+    }
+
+    /** @test */
+    public function adds_a_comment_private_member(): void
+    {
+        $file = <<<PHP
+        <?php
+        class User
+        {
+            private function method(): string
+            {
+                return '';
+            }
+        }
+        PHP;
+
+        $code = (new Annotator(['comment'], ['private']))->process($file);
+
+        self::assertEquals(
+            <<<PHP
+            <?php
+            class User
+            {
+                /**
+                 * comment
+                 */
+                private function method(): string
+                {
+                    return '';
+                }
+            }
+            PHP,
+            $code
+        );
+    }
+
+    /** @test */
+    public function adds_a_comment_protected_member(): void
+    {
+        $file = <<<PHP
+        <?php
+        class User
+        {
+            protected function method(): string
+            {
+                return '';
+            }
+        }
+        PHP;
+
+        $code = (new Annotator(['comment'], ['protected']))->process($file);
+
+        self::assertEquals(
+            <<<PHP
+            <?php
+            class User
+            {
+                /**
+                 * comment
+                 */
+                protected function method(): string
+                {
+                    return '';
+                }
+            }
+            PHP,
+            $code
+        );
+    }
+}
