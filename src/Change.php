fbdfbd689a5fcca51e2ffd279dd5173171fa600b
david_smith <david_smith@sweetwater.com>
2025-02-16 09:45:09 -0500

    feat/annotator Updated `src/Change.php`.

--- src/Change.php
+++ src/Change.php
@@ -8,10 +8,28 @@ class Change
 {
     use DataModel;
 
+    /**
+     * @link https://github.com/zero-to-prod/docblock-annotator
+     */
     public const start = 'start';
+    /**
+     * @link https://github.com/zero-to-prod/docblock-annotator
+     */
     public int $start;
+    /**
+     * @link https://github.com/zero-to-prod/docblock-annotator
+     */
     public const end = 'end';
+    /**
+     * @link https://github.com/zero-to-prod/docblock-annotator
+     */
     public int $end;
+    /**
+     * @link https://github.com/zero-to-prod/docblock-annotator
+     */
     public const text = 'text';
+    /**
+     * @link https://github.com/zero-to-prod/docblock-annotator
+     */
     public string $text;
 }
