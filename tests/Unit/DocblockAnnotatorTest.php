fe3270acc7b1a14c5d6e9050c0d82bc1ddb86c3f
david_smith <david_smith@sweetwater.com>
2025-02-16 08:34:36 -0500

    feat/annotator Created `tests/Unit/DocblockAnnotatorTest.php`.

---
+++ tests/Unit/DocblockAnnotatorTest.php
@@ -0,0 +1,152 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Zerotoprod\DocblockAnnotator\Annotator;
+
+class DocblockAnnotatorTest extends TestCase
+{
+    /** @test */
+    public function adds_a_comment(): void
+    {
+        $file = <<<PHP
+        <?php
+        class User
+        {
+            public function method(): string
+            {
+                return '';
+            }
+        }
+        PHP;
+
+        $code = (new Annotator(['comment']))->process($file);
+
+        self::assertEquals(
+            <<<PHP
+            <?php
+            class User
+            {
+                /**
+                 * comment
+                 */
+                public function method(): string
+                {
+                    return '';
+                }
+            }
+            PHP,
+            $code
+        );
+    }
+
+    /** @test */
+    public function adds_a_comments(): void
+    {
+        $file = <<<PHP
+        <?php
+        class User
+        {
+            public function method(): string
+            {
+                return '';
+            }
+        }
+        PHP;
+
+        $code = (new Annotator(['comment1', 'comment2']))->process($file);
+
+        self::assertEquals(
+            <<<PHP
+            <?php
+            class User
+            {
+                /**
+                 * comment1
+                 * comment2
+                 */
+                public function method(): string
+                {
+                    return '';
+                }
+            }
+            PHP,
+            $code
+        );
+    }
+
+    /** @test */
+    public function updates_a_comments(): void
+    {
+        $file = <<<PHP
+        <?php
+        class User
+        {
+            /**
+             * existing
+             */
+            public function method(): string
+            {
+                return '';
+            }
+        }
+        PHP;
+
+        $code = (new Annotator(['comment']))->process($file);
+
+        self::assertEquals(
+            <<<PHP
+            <?php
+            class User
+            {
+                /**
+                 * existing
+                 * comment
+                 */
+                public function method(): string
+                {
+                    return '';
+                }
+            }
+            PHP,
+            $code
+        );
+    }
+
+    /** @test */
+    public function updates_inline_comments(): void
+    {
+        $file = <<<PHP
+        <?php
+        class User
+        {
+            /** existing */
+            public function method(): string
+            {
+                return '';
+            }
+        }
+        PHP;
+
+        $code = (new Annotator(['comment']))->process($file);
+
+        self::assertEquals(
+            <<<PHP
+            <?php
+            class User
+            {
+                /**
+                 * existing
+                 * comment
+                 */
+                public function method(): string
+                {
+                    return '';
+                }
+            }
+            PHP,
+            $code
+        );
+    }
+}
