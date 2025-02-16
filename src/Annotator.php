d21a5dac9393f7dbbdacad53f3d6c56a08b63322
david_smith <david_smith@sweetwater.com>
2025-02-16 09:40:04 -0500

    feat/annotator Updated `src/Annotator.php`.

--- src/Annotator.php
+++ src/Annotator.php
@@ -9,25 +9,38 @@ use PhpParser\ParserFactory;
 
 class Annotator extends NodeVisitorAbstract
 {
+    public const method = 'method';
+    public const property = 'property';
+    public const constant = 'constant';
+    public const public = 'public';
+    public const private = 'private';
+    public const protected = 'protected';
+
     /** @var Change[] */
     private array $changes = [];
     private array $comments;
+    private array $visibility;
+    private array $members;
 
-    public function __construct(array $comments)
-    {
+    public function __construct(
+        array $comments,
+        array $visibility = [self::public],
+        array $members = [self::method, self::property, self::constant]
+    ) {
         $this->comments = $comments;
+        $this->visibility = array_map('strtolower', $visibility);
+        $this->members = array_map('strtolower', $members);
     }
 
     public function process(string $code): string
     {
-        $parser = (new ParserFactory)->createForHostVersion();
         $traverser = new NodeTraverser();
         $traverser->addVisitor($this);
+        $traverser->traverse(
+            (new ParserFactory)->createForHostVersion()->parse($code)
+        );
 
-        $traverser->traverse($parser->parse($code));
-
-        $changes = array_reverse($this->changes);
-        foreach ($changes as $change) {
+        foreach (array_reverse($this->changes) as $change) {
             $code = substr_replace(
                 $code,
                 $change->text,
@@ -39,7 +52,7 @@ class Annotator extends NodeVisitorAbstract
         return $code;
     }
 
-    private function hasEquivalentComment(string $existing_comment, string $comment): bool
+    private function hasEquivalentCommentLine(string $existing_comment, string $comment): bool
     {
         $normalized = str_replace(' ', '', $comment);
         foreach (explode("\n", $existing_comment) as $line) {
@@ -51,7 +64,7 @@ class Annotator extends NodeVisitorAbstract
         return false;
     }
 
-    private function formatDocComment(string $existing_doc, array $new_comments): string
+    private function format(string $existing_doc, array $new_comments): string
     {
         if (strpos($existing_doc, "\n") === false) {
             $existing_content = trim(substr($existing_doc, 3, -2));
@@ -76,40 +89,96 @@ class Annotator extends NodeVisitorAbstract
         return $doc;
     }
 
-    public function enterNode(Node $node): void
+    private function processComment(Node $node): void
     {
-        if ($node instanceof Node\Stmt\ClassMethod && $node->isPublic()) {
-            $doc_comment = $node->getDocComment();
-            if ($doc_comment) {
-                $existing_doc = $doc_comment->getText();
-                $new_comments = [];
+        $doc_comment = $node->getDocComment();
+        if ($doc_comment) {
+            $existing_doc = $doc_comment->getText();
+            $new_comments = [];
 
-                foreach ($this->comments as $comment) {
-                    if (!$this->hasEquivalentComment($existing_doc, $comment)) {
-                        $new_comments[] = $comment;
-                    }
+            foreach ($this->comments as $comment) {
+                if (!$this->hasEquivalentCommentLine($existing_doc, $comment)) {
+                    $new_comments[] = $comment;
                 }
+            }
 
-                if (!empty($new_comments)) {
-                    $new_doc = $this->formatDocComment($existing_doc, $new_comments);
-                    $this->changes[] = Change::from([
-                        Change::start => $doc_comment->getStartFilePos(),
-                        Change::end => $doc_comment->getEndFilePos(),
-                        Change::text => $new_doc
-                    ]);
-                }
-            } else {
-                $text = "/**\n";
-                foreach ($this->comments as $comment) {
-                    $text .= "     * $comment\n";
-                }
-                $text .= "     */\n    ";
+            if (!empty($new_comments)) {
+                $new_doc = $this->format($existing_doc, $new_comments);
                 $this->changes[] = Change::from([
-                    Change::start => $node->getStartFilePos(),
-                    Change::end => $node->getStartFilePos() - 1,
-                    Change::text => $text
+                    Change::start => $doc_comment->getStartFilePos(),
+                    Change::end => $doc_comment->getEndFilePos(),
+                    Change::text => $new_doc
                 ]);
             }
+        } else {
+            $text = "/**\n";
+            foreach ($this->comments as $comment) {
+                $text .= "     * $comment\n";
+            }
+            $text .= "     */\n    ";
+            $this->changes[] = Change::from([
+                Change::start => $node->getStartFilePos(),
+                Change::end => $node->getStartFilePos() - 1,
+                Change::text => $text
+            ]);
+        }
+    }
+
+    private function hasMatchingVisibility(Node $node): bool
+    {
+        if ($node instanceof Node\Stmt\ClassMethod) {
+            return (
+                (in_array(self::public, $this->visibility, true) && $node->isPublic())
+                || (in_array(self::protected, $this->visibility, true) && $node->isProtected())
+                || (in_array(self::private, $this->visibility, true) && $node->isPrivate())
+            );
+        }
+
+        if ($node instanceof Node\Stmt\Property) {
+            return (
+                (in_array(self::public, $this->visibility, true) && $node->isPublic())
+                || (in_array(self::protected, $this->visibility, true) && $node->isProtected())
+                || (in_array(self::private, $this->visibility, true) && $node->isPrivate())
+            );
+        }
+
+        if ($node instanceof Node\Stmt\ClassConst) {
+            return (
+                (in_array(self::public, $this->visibility, true) && $node->isPublic())
+                || (in_array(self::protected, $this->visibility, true) && $node->isProtected())
+                || (in_array(self::private, $this->visibility, true) && $node->isPrivate())
+            );
+        }
+
+        return false;
+    }
+
+    private function hasMatchingMemberType(Node $node): bool
+    {
+        if ($node instanceof Node\Stmt\ClassMethod) {
+            return in_array(self::method, $this->members, true);
+        }
+
+        if ($node instanceof Node\Stmt\Property) {
+            return in_array(self::property, $this->members, true);
+        }
+
+        if ($node instanceof Node\Stmt\ClassConst) {
+            return in_array(self::constant, $this->members, true);
+        }
+
+        return false;
+    }
+
+    public function enterNode(Node $node): void
+    {
+        if (($node instanceof Node\Stmt\ClassMethod
+                || $node instanceof Node\Stmt\Property
+                || $node instanceof Node\Stmt\ClassConst)
+            && $this->hasMatchingVisibility($node)
+            && $this->hasMatchingMemberType($node)
+        ) {
+            $this->processComment($node);
         }
     }
 }
