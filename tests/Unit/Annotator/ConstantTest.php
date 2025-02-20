9fe752f3b30e5154f4f32c09e8c9e05aad67f43e
david_smith <david_smith@sweetwater.com>
2025-02-20 10:44:19 -0500

    v4 Updated `tests/Unit/Annotator/ConstantTest.php`.

--- tests/Unit/Annotator/ConstantTest.php
+++ tests/Unit/Annotator/ConstantTest.php
@@ -4,296 +4,133 @@ namespace Tests\Unit\Annotator;
 
 use Tests\TestCase;
 use Zerotoprod\DocblockAnnotator\Annotator;
+use Zerotoprod\DocblockAnnotator\Statement;
 
 class ConstantTest extends TestCase
 {
     /** @test */
-    public function adds_a_comment(): void
+    public function adds_comment_to_standalone_constant(): void
     {
         $file = <<<PHP
         <?php
-        class User
-        {
-            public const CONSTANT = 'value';
-        }
+        
+        const STATUS_ACTIVE = 1;
         PHP;
 
-        $code = (new Annotator(['comment']))->process($file);
+        $code = (new Annotator(['Constant comment'], ['public'], [Statement::Const_]))->process($file);
 
         self::assertEquals(
             <<<PHP
             <?php
-            class User
-            {
-                /**
-                 * comment
-                 */
-                public const CONSTANT = 'value';
-            }
-            PHP,
-            $code
-        );
-    }
-
-    /** @test */
-    public function adds_a_comments(): void
-    {
-        $file = <<<PHP
-        <?php
-        class User
-        {
-            public const CONSTANT = 'value';
-        }
-        PHP;
-
-        $code = (new Annotator(['comment1', 'comment2']))->process($file);
-
-        self::assertEquals(
-            <<<PHP
-            <?php
-            class User
-            {
-                /**
-                 * comment1
-                 * comment2
-                 */
-                public const CONSTANT = 'value';
-            }
-            PHP,
-            $code
-        );
-    }
-
-    /** @test */
-    public function updates_a_comments(): void
-    {
-        $file = <<<PHP
-        <?php
-        class User
-        {
+            
             /**
-             * existing
+             * Constant comment
              */
-            public const CONSTANT = 'value';
-        }
-        PHP;
-
-        $code = (new Annotator(['comment']))->process($file);
-
-        self::assertEquals(
-            <<<PHP
-            <?php
-            class User
-            {
-                /**
-                 * existing
-                 * comment
-                 */
-                public const CONSTANT = 'value';
-            }
+            const STATUS_ACTIVE = 1;
             PHP,
             $code
         );
     }
 
     /** @test */
-    public function updates_inline_comments(): void
+    public function adds_multiple_comments_to_standalone_constant(): void
     {
         $file = <<<PHP
         <?php
-        class User
-        {
-            /** existing */
-            public const CONSTANT = 'value';
-        }
+        
+        const STATUS_ACTIVE = 1;
         PHP;
 
-        $code = (new Annotator(['comment']))->process($file);
+        $code = (new Annotator(['Comment 1', 'Comment 2'], ['public'], [Statement::Const_]))->process($file);
 
         self::assertEquals(
             <<<PHP
             <?php
-            class User
-            {
-                /**
-                 * existing
-                 * comment
-                 */
-                public const CONSTANT = 'value';
-            }
-            PHP,
-            $code
-        );
-    }
-
-    /** @test */
-    public function adds_a_comment_public_constant(): void
-    {
-        $file = <<<PHP
-        <?php
-        class User
-        {
-            public const CONSTANT = 'value';
-        }
-        PHP;
-
-        $code = (new Annotator(['comment'], ['public']))->process($file);
-
-        self::assertEquals(
-            <<<PHP
-            <?php
-            class User
-            {
-                /**
-                 * comment
-                 */
-                public const CONSTANT = 'value';
-            }
-            PHP,
-            $code
-        );
-    }
-
-    /** @test */
-    public function does_not_add_a_comment_public_constant(): void
-    {
-        $file = <<<PHP
-        <?php
-        class User
-        {
-            public const CONSTANT = 'value';
-        }
-        PHP;
-
-        $code = (new Annotator(['comment'], ['private']))->process($file);
-
-        self::assertEquals(
-            <<<PHP
-            <?php
-            class User
-            {
-                public const CONSTANT = 'value';
-            }
-            PHP,
-            $code
-        );
-    }
-
-    /** @test */
-    public function adds_a_comment_private_constant(): void
-    {
-        $file = <<<PHP
-        <?php
-        class User
-        {
-            private const CONSTANT = 'value';
-        }
-        PHP;
-
-        $code = (new Annotator(['comment'], ['private']))->process($file);
-
-        self::assertEquals(
-            <<<PHP
-            <?php
-            class User
-            {
-                /**
-                 * comment
-                 */
-                private const CONSTANT = 'value';
-            }
-            PHP,
-            $code
-        );
-    }
-
-    /** @test */
-    public function adds_a_comment_protected_constant(): void
-    {
-        $file = <<<PHP
-        <?php
-        class User
-        {
-            protected const CONSTANT = 'value';
-        }
-        PHP;
-
-        $code = (new Annotator(['comment'], ['protected']))->process($file);
-
-        self::assertEquals(
-            <<<PHP
-            <?php
-            class User
-            {
-                /**
-                 * comment
-                 */
-                protected const CONSTANT = 'value';
-            }
-            PHP,
-            $code
-        );
-    }
-
-    /** @test */
-    public function adds_a_comment_to_multiple_constants(): void
-    {
-        $file = <<<PHP
-        <?php
-        class User
-        {
-            public const CONSTANT_1 = 'value1',
-                         CONSTANT_2 = 'value2';
-        }
-        PHP;
-
-        $code = (new Annotator(['comment']))->process($file);
-
-        self::assertEquals(
-            <<<PHP
-            <?php
-            class User
-            {
-                /**
-                 * comment
-                 */
-                public const CONSTANT_1 = 'value1',
-                             CONSTANT_2 = 'value2';
-            }
-            PHP,
-            $code
-        );
-    }
-
-    /** @test */
-    public function updates_comments_on_multiple_constants(): void
-    {
-        $file = <<<PHP
-        <?php
-        class User
-        {
+            
             /**
-             * existing
+             * Comment 1
+             * Comment 2
              */
-            public const CONSTANT_1 = 'value1',
-                         CONSTANT_2 = 'value2';
-        }
+            const STATUS_ACTIVE = 1;
+            PHP,
+            $code
+        );
+    }
+
+    /** @test */
+    public function updates_existing_standalone_constant_comment(): void
+    {
+        $file = <<<PHP
+        <?php
+        
+        /**
+         * Existing comment
+         */
+        const STATUS_ACTIVE = 1;
         PHP;
 
-        $code = (new Annotator(['comment']))->process($file);
+        $code = (new Annotator(['New comment'], ['public'], [Statement::Const_]))->process($file);
 
         self::assertEquals(
             <<<PHP
             <?php
-            class User
-            {
-                /**
-                 * existing
-                 * comment
-                 */
-                public const CONSTANT_1 = 'value1',
-                             CONSTANT_2 = 'value2';
-            }
+            
+            /**
+             * Existing comment
+             * New comment
+             */
+            const STATUS_ACTIVE = 1;
+            PHP,
+            $code
+        );
+    }
+
+    /** @test */
+    public function ignores_duplicate_standalone_constant_comments(): void
+    {
+        $file = <<<PHP
+        <?php
+        
+        /**
+         * @var int
+         */
+        const STATUS_ACTIVE = 1;
+        PHP;
+
+        $code = (new Annotator(['@var int'], ['public'], [Statement::Const_]))->process($file);
+
+        self::assertEquals(
+            <<<PHP
+            <?php
+            
+            /**
+             * @var int
+             */
+            const STATUS_ACTIVE = 1;
+            PHP,
+            $code
+        );
+    }
+
+    /** @test */
+    public function adds_comment_to_multiple_standalone_constants(): void
+    {
+        $file = <<<PHP
+        <?php
+        
+        const STATUS_ACTIVE = 1, STATUS_INACTIVE = 0;
+        PHP;
+
+        $code = (new Annotator(['Status constants'], ['public'], [Statement::Const_]))->process($file);
+
+        self::assertEquals(
+            <<<PHP
+            <?php
+            
+            /**
+             * Status constants
+             */
+            const STATUS_ACTIVE = 1, STATUS_INACTIVE = 0;
             PHP,
             $code
         );
